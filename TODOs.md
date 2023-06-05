e65ee49bf81cd73d7f127f220f357a22a0ecd39a
Joel Perez <jxp489@gmail.com>
2023-06-05 10:23:09 -0700

    removed some dependencies

---
+++ TODOs.md
@@ -0,0 +1,12 @@
+# Meg TODOs
+Mon Jun 5 2023
+
+## Routes
+1. Login (create session)
+2. Logout (destroy session)
+3. Register/Signup (create session,redirect to home page body:{
+    email: user@mail.com
+    password: 123456 (bcrypt model hash etc)
+} )
+
+
--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const passport = require("passport");
-require("./config/passport")(passport);
 const path = require("path");
 const routes = require("./controllers");
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 // static files
 app.use(express.static(path.join(__dirname, "public")));
