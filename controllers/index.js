3a07044a25a23f48be30537c3d079b2c0db37af6
Joel Perez <jxp489@gmail.com>
2023-06-06 11:57:55 -0700

    added backend dashboard and used bootstrap to theme it

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,18 @@ const login = require("./login");
 const logout = require("./logout");
 const signup = require("./signup");
 
-router.get("/", (req, res) => {
-  try {
-    res.send("MEG APP Coming SOON!");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/", (req, res) => {
+//   try {
+//     res.send(`
+//     <h1 class="bg-dark">Home</h1>
+//     <a class="button" href="/login">Login</a>
+//     <a href="/logout">Logout</a>
+//     <a href="/signup">Signup</a>
+//     `);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.use("/login", login);
 router.use("/logout", logout);
