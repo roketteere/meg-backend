7c2f4147cd1593e9a11b238fc67bf1d0d6ced962
Joel Perez <jxp489@gmail.com>
2023-06-07 14:29:40 -0700

    served static maps.html to test realtime location

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,11 @@ const logout = require("./logout");
 const signup = require("./signup");
 const profile = require("./profile");
 const API = require("./api");
+const path = require("path");
 
+router.get("/maps", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/maps.html"));
+});
 router.use("/login", login);
 router.use("/logout", logout);
 router.use("/signup", signup);
