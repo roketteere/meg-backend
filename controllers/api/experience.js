c280d2ff9f1ed03e9c498b28e736cd6e06a4ba39
Joel Perez <jxp489@gmail.com>
2023-06-14 18:57:26 -0700

    added post route to get all experiences, pushing, deploying, testing live route

--- controllers/api/experience.js
+++ controllers/api/experience.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {Experience} = require("../../models");
+const { Experience } = require("../../models");
 
 router.post("/", async (req, res) => {
   try {
@@ -11,4 +11,17 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/get", async (req, res) => {
+  try {
+    const experienceData = await Experience.findAll({
+      where: {
+        userId: req.body.userId,
+      },
+    });
+    res.status(200).json(experienceData);
+  } catch (err) {
+    console.log(`Error Getting Experiences: ${err}`);
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
