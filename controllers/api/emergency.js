30a5e5717f659239508b88227e74086cc3766d80
Joel Perez <jxp489@gmail.com>
2023-06-14 18:44:12 -0700

    added get all emergency contacts route, pushing to test deployed

--- controllers/api/emergency.js
+++ controllers/api/emergency.js
@@ -11,5 +11,19 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const emergencyData = await EmergencyContact.findAll({
+      where: {
+        userId: req.body.userId,
+      },
+    });
+    res.status(200).json(emergencyData);
+  } catch (err) {
+    console.log(`Error Getting Emergency Contacts: ${err}`);
+    res.status(500).json(err);
+  }
+});
+
 // Export the router
 module.exports = router;
